0fec4002b8d53b8ed98d0417305a627d28dcf16f
Curtis Rasmussen <crasmuss@alumni.uoguelph.ca>
2020-09-28 17:01:40 -0400

    Complete jaden-case kata

--- src/codewars/core.clj
+++ src/codewars/core.clj
@@ -1,6 +1,9 @@
-(ns codewars.core)
+(ns codewars.core
+  (:require [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; Your task is to convert strings to how they would be written by Jaden Smith.
+;; The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+;; Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+;; Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+(defn jaden-case [s]
+  (str/join " " (map str/capitalize (str/split s #" "))))
